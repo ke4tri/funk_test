f7fbf6f048b2b21dc1037b434a2171b80be49118
Wayne Collier <ke4tri@gmail.com>
2018-09-07 13:26:37 -0500

    got the function/button working now move to translator repo

--- main.js
+++ main.js
@@ -4,9 +4,15 @@ const printToDom = (stringToPrint, divId) => {
     selectedDiv.innerHTML += stringToPrint;
 };
 
-function translate() {
-    console.log('Test of function');
-    var text_fun = document.getElementById("inText").value;
-    
-    return printToDom(text_fun, 'dom');
-};
+// let translate = () =>  {
+//     console.log('Test of function');
+//     var text_fun = document.getElementById("inText").value;
+//     return printToDom(text_fun, 'dom');
+// };
+
+
+document.getElementById("funkBut").addEventListener("click", function(){
+    let x = document.getElementById("inText").value;
+    document.getElementById("demo").innerHTML = x;
+});
+
